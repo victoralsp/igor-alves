ce53b3dac869d0bee78e33cf410c7d8730b864cc
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2025-01-19 21:17:17 -0300

    Menu responsivo finalizado

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const iconeMenu = document.getElementById('icone-menu')
+const navegacaoMenu = document.getElementById('ul-nav-header')
+const headerShadow = document.getElementById('header')
+let menuAberto = true
+
+iconeMenu.addEventListener('click', ()=> {
+  if (menuAberto) {
+    navegacaoMenu.classList.add('active')
+    iconeMenu.innerHTML = '<i class="fa-solid fa-x"></i>'
+  } else {
+    navegacaoMenu.classList.remove('active')
+    iconeMenu.innerHTML = '<i class="fa-solid fa-bars"></i>'
+  }
+  menuAberto = !menuAberto
+})
+
+
 var swiper = new Swiper(".swiper", {
     cssMode: true,
     loop: true,
