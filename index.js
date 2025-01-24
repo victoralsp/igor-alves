1167e6bdadc69ac2b2c2d6fbc91c7ed256fd7f74
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2025-01-23 21:42:12 -0300

    Finalizada a responsividade das sections: inicial, apresentando e sobre mim.

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const iconeMenu = document.getElementById('icone-menu')
 const navegacaoMenu = document.getElementById('ul-nav-header')
 const headerShadow = document.getElementById('header')
-let menuAberto = true
+const linksNavbar = document.querySelectorAll('.links-navegacao-header')
+let menuAberto = false
 
 iconeMenu.addEventListener('click', ()=> {
   if (menuAberto) {
-    navegacaoMenu.classList.add('active')
-    iconeMenu.innerHTML = '<i class="fa-solid fa-x"></i>'
-  } else {
     navegacaoMenu.classList.remove('active')
     iconeMenu.innerHTML = '<i class="fa-solid fa-bars"></i>'
+  } 
+  else {
+    navegacaoMenu.classList.add('active')
+    iconeMenu.innerHTML = '<i class="fa-solid fa-x"></i>'
   }
   menuAberto = !menuAberto
 })
-
 
 var swiper = new Swiper(".swiper", {
     cssMode: true,
